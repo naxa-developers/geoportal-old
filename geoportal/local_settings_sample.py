3c2bcfad7886d5630dd1e6e6751d38d6101e0678
raeesaroj <raesaroj16@gmail.com>
2020-04-21 18:03:00 +0545

    local settings update

--- geoportal/local_settings_sample.py
+++ geoportal/local_settings_sample.py
@@ -10,6 +10,11 @@ DATABASES = {
          'PORT': '5432',
      }
  }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+
+}
 
 CORS_ORIGIN_WHITELIST = [
     "https://example.com",
